98c957d418d8ce832594e6b376fbc07bb68105f2
Hemang Pandhi <hemang.pandhi@gmail.com>
2026-05-28 15:09:13 +0900

    Remove EXTRA_PARTIAL_RESULTS from AssistantSession to fix STT latency

--- app/src/main/java/com/example/gemininano/AssistantSession.kt
+++ app/src/main/java/com/example/gemininano/AssistantSession.kt
@@ -107,7 +107,6 @@ class AssistantSession(context: Context) : VoiceInteractionSession(context), Tex
         val speechRecognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
             putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
             putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
-            putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true)
         }
 
         speechRecognizer?.setRecognitionListener(object : RecognitionListener {
